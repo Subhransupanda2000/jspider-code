f06a8f79463a7139db891d2c76e3c1aab3ce13e5
Subhransupanda2000 <imsubhransupanda2000@gmail.com>
2024-05-30 22:16:26 +0530

    first commit

---
+++ src/main/java/com/example/jspider_java/Circle.java
@@ -0,0 +1,21 @@
+package com.example.jspider_java;
+
+public class Circle {
+    public static void main(String[] args) {
+        int radious=5;
+        int diameter=diameterOfCircle(radious);
+        System.out.println(diameter);
+        double circumferenceOfCircle=circumferenceOfCircle(radious);
+        System.out.println(circumferenceOfCircle);
+    }
+    public  static int diameterOfCircle(int radious)
+    {
+        int result=2*radious;
+        return result;
+    }
+    public  static double circumferenceOfCircle(int radious)
+    {
+        double result=2*Math.PI*radious;
+        return result;
+    }
+}
