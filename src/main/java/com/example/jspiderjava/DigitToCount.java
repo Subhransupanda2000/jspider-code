5467d5f1f21bd0422734209644b1969998162a2e
Subhransupanda2000 <imsubhransupanda2000@gmail.com>
2024-06-05 16:21:38 +0530

    Digit count modified

--- src/main/java/com/example/jspiderjava/DigitToCount.java
+++ src/main/java/com/example/jspiderjava/DigitToCount.java
@@ -1,12 +1,8 @@
 package com.example.jspiderjava;
 
-public class ElementOuccrence {
-    public static void main(String[] args) {
-        int n = 10;
-        int result = ouccuranceOfelements(n);
-        System.out.println(result);
-    }
-    private static int ouccuranceOfelements(int n) {
+public class DigitToCount {
+
+    public static int digitToCount(int n) {
         int count = 0;
         n = n * n * n;
         int ouccurance = 0;
---
+++ src/test/java/org/example/TestDigitToCount.java
@@ -0,0 +1,15 @@
+package org.example;
+import com.example.jspiderjava.DigitToCount;
+import org.junit.Test;
+import java.util.List;
+import static org.junit.Assert.assertEquals;
+public class TestDigitToCount {
+    @Test
+    public void findLeaderTest() {
+        int n = 10;
+        int actual=DigitToCount.digitToCount(n);
+        int expected=3 ;
+        assertEquals(expected,actual);
+    }
+}
+
