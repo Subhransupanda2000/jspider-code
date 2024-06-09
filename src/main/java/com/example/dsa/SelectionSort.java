58ddabc06d588ce6886847eaffed190235222e03
Subhransupanda2000 <imsubhransupanda2000@gmail.com>
2024-06-09 10:34:37 +0530

    sorting array

--- src/main/java/com/example/dsa/SelectionSort.java
+++ src/main/java/com/example/dsa/SelectionSort.java
@@ -2,15 +2,8 @@ package com.example.dsa;
 
 import java.util.Arrays;
 
-public class selectionSort {
-    public static void main(String[] args) {
-        int[] arr = {4, 8, 7, 2, 9, 1, 3, 6};
-        int[] arr1 = sortedarray(arr);
-        System.out.println(Arrays.toString(arr1));
-
-    }
-
-    private static int[] sortedarray(int[] arr) {
+public class SelectionSort {
+    public static int[] sortedarray(int[] arr) {
         int temp = 0;
         for (int i = 0; i <= arr.length - 1; i++) {
             int min = i;
